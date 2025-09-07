80ab8cd8caf740398d983e6c8b5c4cc93647608a
Rasz_pl <citizenr@gmail.com>
2025-09-07 05:21:40 +0200

    CI

--- ci/oneHDDsector.bat
+++ ci/oneHDDsector.bat
@@ -2,20 +2,23 @@ echo off
 SET "MFM_HDDdataOneSector=_erase_"
 SET "test=_erase_"
 
-REM generate reference file and CRC
+REM Generate reference file and CRC
 IF "%1"=="-r" (
-	ECHO generate reference MFM_HDDdataOneSector.7z file
-	"C:\Program Files\sigrok\sigrok-cli\sigrok-cli" -D -i ..\test\MFM_HDDdataOneSector.sr -P mfm:dsply_pfx=yes:dsply_sn=yes | "C:\Program Files\7-Zip\7z" a -si MFM_HDDdataOneSector.7z
+	ECHO Generate reference MFM_HDDdataOneSector.7z file
 
-    ECHO generating reference MFM_HDDdataOneSector.crc
+	"C:\Program Files\sigrok\sigrok-cli\sigrok-cli" -D -i ..\test\MFM_HDDdataOneSector.sr -P mfm:dsply_pfx=yes:dsply_sn=yes | "C:\Program Files\7-Zip\7z" u -siMFM_HDDdataOneSector MFM_HDDdataOneSector.7z
+
+    ECHO Ggenerating reference MFM_HDDdataOneSector.crc
 	"C:\Program Files\7-Zip\7z" l -slt MFM_HDDdataOneSector.7z |findstr /b CRC |for /f "tokens=3 delims= " %%i in ('more') do @echo %%i > MFM_HDDdataOneSector.crc
+	
+	exit /b
 )
 
-REM read reference MFM_HDDdataOneSector.crc
+REM Read reference MFM_HDDdataOneSector.crc
 set /p MFM_HDDdataOneSector=<MFM_HDDdataOneSector.crc
 set MFM_HDDdataOneSector=%MFM_HDDdataOneSector: =%
 
-REM generate test.crc
+REM Generate test.crc
 "C:\Program Files\sigrok\sigrok-cli\sigrok-cli" -D -i ..\test\MFM_HDDdataOneSector.sr -P mfm:dsply_pfx=yes:dsply_sn=yes |"C:\Program Files\7-Zip\7z" h -si |findstr data |for /f "tokens=4 delims= " %%G in ('more') do @echo %%G> test.crc
 set /p test=<test.crc
 set test=%test: =%
